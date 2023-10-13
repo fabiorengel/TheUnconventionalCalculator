fad584469f94658fe2e8e5d1f11e3b1b704b397e
Fábio Rengel <fabiorengel@gmail.com>
2023-10-13 19:29:08 -0300

    Changes untill Class 24
    app.js created and first variables and operations used in it.
    
    <script> added to index to call js files.

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+//alert('this works!');
+
+const defaultResult = 0;
+
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = '('+ defaultResult + ' + 10) * 3 / 2 - 1';
+
+outputResult(currentResult, calculationDescription);
