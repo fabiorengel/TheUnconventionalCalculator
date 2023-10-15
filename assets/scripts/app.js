d14417941dd90ef74c979456aa3b15ceedc0038e
Fábio Rengel <fabiorengel@gmail.com>
2023-10-15 12:38:44 -0300

    Up to Class 49
    Lessons of:
    > typeOf
    > Array
    >Object
    >console.log()
    
    >>> '-',  '/',  '* '  functions added to our Unconventional Calculator

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,66 @@
 const   defaultResult = 0;
 
 let     currentResult = defaultResult;
+let     logEntries = [];
+
 
 function getUserNumberInput() {
     return parseInt(userInput.value);
 }
+
+function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber} `
+    outputResult(currentResult, calcDescription);
+}
+
+function writeTolog(
+    operationIdentifier,
+    prevResult,
+    operationNumber, 
+    newResult
+){
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);    
+}
+
 function add() {
     const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    const initialResult = currentResult;
     currentResult = currentResult + enteredNumber;
-    outputResult(currentResult, calcDescription);
+    createAndWriteLog('+', initialResult, enteredNumber);
+    writeTolog('+', initialResult, enteredNumber, currentResult);
 };
+function subtract(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteLog('-', initialResult, enteredNumber);    
+    writeTolog('-', initialResult, enteredNumber, currentResult);
+}
+
+function multiply(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteLog('*', initialResult, enteredNumber);    
+    writeTolog('*', initialResult, enteredNumber, currentResult);
+}
+
+function divide(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteLog('/', initialResult, enteredNumber); 
+    writeTolog('/', initialResult, enteredNumber, currentResult);   
+}
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
